0117f5b0b28448380691c3391c63c87bcee5d9a4
Ville Vaara <villepvaara@gmail.com>
2017-09-26 09:00:06 +0300

    updated intros.py to take encoding into account

--- 2_basic_concepts/intros.py
+++ 2_basic_concepts/intros.py
@@ -1,5 +1,5 @@
 def read_txt_file_to_list(filename):
-    with open(filename, 'r') as txtfile:
+    with open(filename, 'r', encoding='utf-8') as txtfile:
         lines = []
         for line in txtfile.readlines():
             lines.append(line)
@@ -7,7 +7,7 @@ def read_txt_file_to_list(filename):
 
 
 def write_lines_into_txt_file(filename, lines, add_newlines=False):
-    with open(filename, 'w') as txtfile:
+    with open(filename, 'w', encoding='utf-8') as txtfile:
         for line in lines:
             line = str(line)
             if add_newlines:
